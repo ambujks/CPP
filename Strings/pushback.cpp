ee9ba3ee6cd3d814dc5f04093ca99d61f439dafd
ambujks <ambujks2002@gmail.com>
2025-11-27 17:13:26 +0530

    member function/method in string

---
+++ Strings/pushback.cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+#include<string>
+
+using namespace std;
+int main()
+{
+    string a = "Ambuj";
+    cout << a << endl;
+    cout << "Lenght of the string is : " <<a.length() << endl;
+
+    a.clear();
+    cout << a << endl << a.length() << endl;
+
+    // a.append(" Kumar"); //to add character/string to existing.
+    // cout << a << endl;
+    // cout << "Lenght of the string is : " <<a.length() << endl;
+
+
+    
+
+
+    // a.push_back('a');  //add character at last
+    // cout << a << endl;
+
+    // a.pop_back();
+    // cout << a << endl; //remove character from last
+    
+
+    return 0;
+}
