4d2ddcfec60d13fcdd71cc5ae565bc3dd0d5550a
ambujks <ambujks2002@gmail.com>
2025-11-27 17:14:32 +0530

    updated member function/ method of string.

--- Strings/pushback.cpp
+++ Strings/pushback.cpp
@@ -8,22 +8,22 @@ int main()
     cout << a << endl;
     cout << "Lenght of the string is : " <<a.length() << endl;
 
-    a.clear();
+    a.clear(); //to clear the string.
     cout << a << endl << a.length() << endl;
 
-    // a.append(" Kumar"); //to add character/string to existing.
-    // cout << a << endl;
-    // cout << "Lenght of the string is : " <<a.length() << endl;
+    a.append(" Kumar"); //to add character/string to existing.
+    cout << a << endl;
+    cout << "Lenght of the string is : " <<a.length() << endl;
 
 
     
 
 
-    // a.push_back('a');  //add character at last
-    // cout << a << endl;
+    a.push_back('a');  //add character at last
+    cout << a << endl;
 
-    // a.pop_back();
-    // cout << a << endl; //remove character from last
+    a.pop_back();
+    cout << a << endl; //remove character from last
     
 
     return 0;
